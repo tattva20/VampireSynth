b3f3a2e44100c075a0df209ce4a92c7a35ae596c
Octavio Rojas <octavio.rojas@pluto.tv>
2024-08-29 23:18:50 -0600

    Initial Commit

---
+++ Force1/Force1App.swift
@@ -0,0 +1,17 @@
+//
+//  Force1App.swift
+//  Force1
+//
+//  Created by Octavio Rojas on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Force1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
